68dc9192223c91804c5baef94e13c16d58f34324
Kobi <hi@kaitiger.tech>
2020-09-27 15:55:06 -0400

    Added media type argument

--- commands/add.js
+++ commands/add.js
@@ -7,11 +7,25 @@ module.exports = {
   description: 'Add a Torrent',
   execute (msg, args) {
     const url = args[0]
+    const type = args[1]
+
+    let options = {}
+
+    if (type) {
+      const typeDir = {
+        tv: 'tv-shows',
+        movie: 'movies',
+        music: 'music'
+      }
+      options = { 'download-dir': `${process.env.DOWNLOAD_ROOT_PATH}/${typeDir[type.toLowerCase()]}` }
+      msg.channel.send(`Downloading to ${typeDir[type.toLowerCase()]}`)
+    }
+
     // if (url.includes('https://thepiratebay.org')) {
     //   const pirateBayListing = await PirateBay.getTorrent(url.replace(/^\D+/g, ''))
     //   console.info(pirateBayListing)
     // }
-    server.addUrl(url, (err, result) => {
+    server.addUrl(url, options, (err, result) => {
       if (err) {
         console.error(err)
         msg.channel.send('`[sad pirate noise]` There was a problem adding your torrent URL')
